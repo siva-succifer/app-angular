26f2cf151a4a7613c6966dcf6423b4e4c0fd37e9
siva-succifer <siva.succifer@gmail.com>
2019-05-15 23:03:11 +0530

    Initiated Basic Angular Project

---
+++ src/app/demo/demo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+selector: 'app-demo',
+templateUrl: './demo.component.html'
+})
+export class DemoComponent {
+  name = 'Siva';
+component() {
+
+}
+}
